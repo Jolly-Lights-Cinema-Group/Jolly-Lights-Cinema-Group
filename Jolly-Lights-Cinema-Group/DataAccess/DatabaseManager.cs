f677aa2f5887f59a1ba171b600b997f7b4f523eb
lois <lois@procolix.eu>
2025-04-15 14:39:40 +0200

    Add DatabaseManager

--- Jolly-Lights-Cinema-Group/DataAccess/DatabaseManager.cs
+++ Jolly-Lights-Cinema-Group/DataAccess/DatabaseManager.cs
@@ -1,14 +1,21 @@
 using Microsoft.Data.Sqlite;
 using System;
+using System.Dynamic;
 using System.IO;
 
 namespace JollyLightsCinemaGroup.DataAccess
 {
     public class DatabaseManager
     {
-        private static readonly string _dbPath = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "Database", "cinema.db");
-        private static readonly string _connectionString = $"Data Source={_dbPath}";
-
+        private static string _dbPath = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "Database", "cinema.db");
+        private static string _schemaPath = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "Database", "schema.sql");
+        private static string _connectionString = $"Data Source={_dbPath}";
+        public static void OverridePaths(string dbDirectory)
+        {
+            _dbPath = Path.Combine(dbDirectory, "cinema.db");
+            _schemaPath = Path.Combine(dbDirectory, "schema.sql");
+            _connectionString = $"Data Source={_dbPath}";
+        }
         public static SqliteConnection GetConnection()
         {
             return new SqliteConnection(_connectionString);
@@ -31,8 +38,7 @@ namespace JollyLightsCinemaGroup.DataAccess
             {
                 connection.Open();
                 var command = connection.CreateCommand();
-                string schemaPath = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "Database", "schema.sql");
-                command.CommandText = File.ReadAllText(schemaPath);
+                command.CommandText = File.ReadAllText(_schemaPath);
                 command.ExecuteNonQuery();
                 Console.WriteLine("Database initialized.");
             }
