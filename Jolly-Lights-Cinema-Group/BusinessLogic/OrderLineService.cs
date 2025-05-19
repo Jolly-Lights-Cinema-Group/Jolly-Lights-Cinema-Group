c7b0c8774b0baa585cb138315028cccc7cd7bd7f
Lois Verheij <loisverheij@MacBook-Air-van-Lois.local>
2025-05-19 20:51:03 +0200

    Start creating order line for reserved seats

--- Jolly-Lights-Cinema-Group/BusinessLogic/OrderLineService.cs
+++ Jolly-Lights-Cinema-Group/BusinessLogic/OrderLineService.cs
@@ -1,3 +1,4 @@
+using Jolly_Lights_Cinema_Group.Enum;
 using JollyLightsCinemaGroup.DataAccess;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,41 @@ public class OrderLineService
         _orderLineRepo.AddOrderLine(orderLine);
     }
     public void CreateOrderLineForReservation(Reservation reservation)
+    {
+        CreateOrderLineForScheduleShopItem(reservation);
+        CreateOrderLineForScheduleSeats(reservation);
+    }
+
+    public void CreateOrderLineForScheduleSeats(Reservation reservation)
+    {
+        ScheduleSeatRepository scheduleSeatRepository = new();
+        SeatRepository seatRepository = new();
+
+        List<ScheduleSeat> scheduleSeats = scheduleSeatRepository.GetSeatsByReservation(reservation);
+
+        List<IGrouping<SeatType, ScheduleSeat>> groupedSeats = scheduleSeats
+            .GroupBy(seat => seat.Type)
+            .ToList();
+
+        // foreach (IGrouping<SeatType, ScheduleSeat> group in groupedSeats)
+        // {
+        //     SeatType type = group.Key;
+        //     int quantity = group.Count();
+
+        //     ShopItem shopItem = shopItemRepository.GetShopItemById(shopItemId)!;
+        //     if (shopItem == null)
+        //         continue;
+
+        //     double totalPrice = quantity * shopItem.Price;
+        //     int vatPercentage = 21;
+
+        //     OrderLine orderLine = new OrderLine((int)reservation.Id!, quantity, shopItem.Name, vatPercentage, totalPrice);
+
+        //     _orderLineRepo.AddOrderLine(orderLine);
+        // }
+    }
+
+    public void CreateOrderLineForScheduleShopItem(Reservation reservation)
     {
         ScheduleShopItemRepository scheduleShopItemRepository = new();
         ShopItemRepository shopItemRepository = new();
